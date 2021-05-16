701d2e5ede261f0c53b1dec2bf44600bf07e4110
Keith Noguchi <keith.noguchi@gmail.com>
2021-05-16 11:51:48 -0700

    client/01-building-blocks/06-build-function: Pass argument

--- client/01-building-blocks/06-build-function.js
+++ client/01-building-blocks/06-build-function.js
@@ -1,13 +1,33 @@
-document.querySelector('button').onclick = () => {
+buttons = document.querySelectorAll('button')
+buttons[0].onclick = () => {
+    displayMessage("Hey yo!", 'standard')
+}
+buttons[1].onclick = () => {
+    displayMessage("Oops!", 'warning')
+}
+buttons[2].onclick = () => {
+    displayMessage("let's chat!", 'chat')
+}
+
+function displayMessage(msgText, msgType) {
     const html = document.querySelector('html')
     const panel = document.createElement('div')
     panel.setAttribute('class', 'msgBox')
     html.appendChild(panel)
 
     const msg = document.createElement('p')
-    msg.textContent = 'This is a message box'
+    msg.textContent = msgText
     panel.appendChild(msg)
 
+    if (msgType === 'warning') {
+        msg.style.backgroundImage = 'url(icons/warning.png)'
+        panel.style.backgroundColor = 'red'
+    } else if (msgType === 'chat') {
+        msg.style.backgroundImage = 'url(icons/chat.png)'
+        panel.style.backgroundColor = 'aqua'
+    } else {
+        msg.style.paddingLeft = '20px'
+    }
     const close = document.createElement('button')
     close.textContent = 'x'
     close.onclick = () => {
